696dfb7a567c8054589184b39f6fc0bcecdc2a65
23077875 <23077875@siswa.um.edu.my>
2025-04-20 09:22:31 +0000

    Week 5

--- phoneradar/main/urls.py
+++ phoneradar/main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='home'),  # Handles the root URL
     path('index/', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
